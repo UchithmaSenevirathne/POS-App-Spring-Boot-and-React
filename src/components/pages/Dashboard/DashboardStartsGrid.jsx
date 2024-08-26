4e963b537a280811ee4a43810417697c4c09f61f
UchithmaSenevirathne <uchithma@gmail.com>
2024-08-26 17:09:13 +0530

    change color when selected

--- src/components/pages/Dashboard/DashboardStartsGrid.jsx
+++ src/components/pages/Dashboard/DashboardStartsGrid.jsx
@@ -12,15 +12,26 @@ import pizza from '../../assets/images/pizza.png'
 import ice from '../../assets/images/ice-cream.png'
 import { BiCake } from 'react-icons/bi'
 import './PopularProducts'
+import { useState } from 'react'
 
 function DashboardStartsGrid({ categories, setSelectedCategory }) {
+
+    const [selectedCategory, setSelectedCategoryLocal] = useState(null);
+
+    const handleCategorySelect = (categoryName) => {
+      setSelectedCategoryLocal(categoryName);
+      setSelectedCategory(categoryName);
+    };
+    
   return (
     <>
     <h1 className='ml-1 font-bold text-[18px] pb-5'>Explore Categories</h1>
     
     <div className="grid w-full grid-cols-5 gap-4 pb-4">
         {categories.map((category, index) => (
-          <BoxWrapper key={index} onClick={() => setSelectedCategory(category.name)}>
+          <BoxWrapper key={index}
+          onClick={() => handleCategorySelect(category.name)}
+          isSelected={selectedCategory === category.name}>
             <div className='flex items-center justify-center w-12 h-12 bg-[#EEF2F5] rounded-md'>
               <img src={category.image} alt={category.name} />
             </div>
@@ -130,6 +141,15 @@ function DashboardStartsGrid({ categories, setSelectedCategory }) {
 
 export default DashboardStartsGrid
 
-function BoxWrapper({ children, onClick  }) {
-    return <div className="flex items-center flex-1 p-4 bg-white hover:bg-[#fff3dd] active:bg-[#fff3dd] border rounded-md hover:border-[orange]" onClick={onClick}>{children}</div>
+function BoxWrapper({ children, onClick, isSelected  }) {
+    return (
+        <div
+          className={`flex items-center flex-1 p-4 border rounded-md cursor-pointer ${
+            isSelected ? 'bg-[#fff3dd] border-[orange]' : 'bg-white hover:bg-[#fff3dd] hover:border-[orange] transition-all duration-500'
+          }`}
+          onClick={onClick}
+        >
+          {children}
+        </div>
+      );
 }
